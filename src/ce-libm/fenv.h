bb508034e062e030960d4b73f1702c3802bc0d5a
Jordan D. (Joda) <joda@odoo.com>
2024-05-17 09:27:31 +0200

    Fix wasm support
    source: https://chromium.googlesource.com/external/github.com/WebAssembly/musl/+/refs/heads/libc_wasm32_clang/arch/wasm32/bits/fenv.h

--- src/ce-libm/fenv.h
+++ src/ce-libm/fenv.h
@@ -50,18 +50,17 @@
 
 #elif defined(__wasm)
 
-#define FE_INEXACT 1
-#define FE_UNDERFLOW 2
-#define FE_OVERFLOW 4
-#define FE_DIVBYZERO 8
-#define FE_INVALID 16
+#define FE_INVALID 1
+#define FE_DIVBYZERO 4
+#define FE_OVERFLOW 8
+#define FE_UNDERFLOW 16
 
-#define FE_ALL_EXCEPT 31
-
+#define FE_INEXACT 32
+#define FE_ALL_EXCEPT 63
 #define FE_TONEAREST 0
-#define FE_TOWARDZERO 1
-#define FE_DOWNWARD 2
-#define FE_UPWARD 3
+#define FE_DOWNWARD 0x400
+#define FE_UPWARD 0x800
+#define FE_TOWARDZERO 0xc00
 
 #else
 #error Unknown architecture
